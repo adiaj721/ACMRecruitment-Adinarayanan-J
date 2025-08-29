1853227ef591f6358bc056c9dfa9ea0e28064f5a
adiaj721 <adiaj721@gmail.com>
2025-08-29 20:51:30 +0530

    Add files via upload

---
+++ Questline-3/fizzbuzz.py
@@ -0,0 +1,65 @@
+Python 3.13.5 (tags/v3.13.5:6cb20a2, Jun 11 2025, 16:15:46) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> 
+>>> 
+>>> #fizzbuzz
+>>> for i in range(1, 51):
+...     if i % 3 == 0 and i % 5 == 0:
+...         print("FizzBuzz")
+...     elif i % 3 == 0:
+...         print("Fizz")
+...     elif i % 5 == 0:
+...         print("Buzz")
+...     else:
+...         print(i)
+... 
+1
+2
+Fizz
+4
+Buzz
+Fizz
+7
+8
+Fizz
+Buzz
+11
+Fizz
+13
+14
+FizzBuzz
+16
+17
+Fizz
+19
+Buzz
+Fizz
+22
+23
+Fizz
+Buzz
+26
+Fizz
+28
+29
+FizzBuzz
+31
+32
+Fizz
+34
+Buzz
+Fizz
+37
+38
+Fizz
+Buzz
+41
+Fizz
+43
+44
+FizzBuzz
+46
+47
+Fizz
+49
+Buzz
